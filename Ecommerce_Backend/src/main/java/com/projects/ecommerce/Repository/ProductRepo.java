fa73ac946aa03cf1650f1623b049cb88a7a79ac3
Faiz Arfi <faizarfi12@gmail.com>
2025-06-19 15:42:12 +0530

    removed unused import

--- Ecommerce_Backend/src/main/java/com/projects/ecommerce/Repository/ProductRepo.java
+++ Ecommerce_Backend/src/main/java/com/projects/ecommerce/Repository/ProductRepo.java
@@ -1,6 +1,5 @@
 package com.projects.ecommerce.Repository;
 
-import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
--- Ecommerce_Backend/src/main/java/com/projects/ecommerce/Services/ProductService.java
+++ Ecommerce_Backend/src/main/java/com/projects/ecommerce/Services/ProductService.java
@@ -1,6 +1,5 @@
 package com.projects.ecommerce.Services;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
