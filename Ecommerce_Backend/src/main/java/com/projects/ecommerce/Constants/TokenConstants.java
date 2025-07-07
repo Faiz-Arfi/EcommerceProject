c54c5a55383abd88e4dcec627010587fb5a0f926
Faiz Arfi <faizarfi12@gmail.com>
2025-07-07 17:39:17 +0530

    increased the access and refresh token validity time

--- Ecommerce_Backend/src/main/java/com/projects/ecommerce/Constants/TokenConstants.java
+++ Ecommerce_Backend/src/main/java/com/projects/ecommerce/Constants/TokenConstants.java
@@ -4,6 +4,6 @@ import lombok.Getter;
 
 @Getter
 public class TokenConstants {
-    public static final int accessTokenValidityMin = 1;
-    public static final int refreshTokenValidityMin = 2;
+    public static final int accessTokenValidityMin = 60;
+    public static final int refreshTokenValidityMin = 120;
 }
